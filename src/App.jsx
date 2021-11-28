ef4dcc0e2d71efad606ecea43260686e1a720aeb
asrbekhasanov <asrbek.hasanov@gmail.com>
2021-11-28 18:18:11 +0500

    second init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import './App.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import About from './Components/Pages/About/About'
+import Home from './Components/Pages/Home/Home'
 
 function App() {
   return (
-    <>app</>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about'>About</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </>
   );
 }
 
